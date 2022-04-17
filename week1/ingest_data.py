6fb81add02d88f633977719c958b1687334754bf
rafzul <rafi.zulfahmi@mail.ugm.ac.id>
2022-04-17 22:17:54 +0700

    selesein main function

--- week1/ingest_data.py
+++ week1/ingest_data.py
@@ -24,14 +24,36 @@ def main(params):
     #create sqlalchemy postgres engine `database://user:password@host:port/database_name`
     engine = create_engine(f'postgresql//{user}:{password}@{host}:{port}/{db}')
     #create iterator to read csv as chunk
-    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
-    deef = next(df_iter)
+    deef_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    #get first chunk
+    deef = next(deef_iter)
     #convert pickup time and dropoff time column to timestamps
     deef.tpep_pickup_datetime = pd.to_datetime(deef.tpep_pickup_datetime)
     deef.tpep_dropoff_datetime = pd.to_datetime(deef.tpep_dropoff_datetime)
-    #get schema 
-    tableschema = pd.io.sql.get_schema(deef,name=table_name)
-    #finally, create table in the database
+    #finally, create table in the database (empty table first, generate only the column). schema is inferred from the fetched df
+    deef.head(0).to_sql(name=table_name, con=engine, if_exists='replace')
+    #get the iteration to get the first chunk of CSV and append to table
+    deef.to_sql(name=table_name, con=engine, if_exists='append')
+
+    #start the loop to get all chunks of csv
+    while True:
+        try:
+            #get starting time 
+            t_start = time()
+            #get next chunks
+            deef = next(deef_iter)
+            #convert pickup time & dropoff time column type of next chunk
+            deef.tpep_pickup_datetime = pd.to_datetime(deef.tpep_pickup_datetime)
+            deef.tpep_dropoff_datetime = pd.to_do_datetime(deef.tpep_dropoff_datetime)
+            #get the iteration to get next chunk of CSV and append to table 
+            deef.to_sql(name=table_name, con=engine, if_exists='append')
+            #get end time & print log messages
+            t_end = time()
+            print('inserted another chunk, took %.3f second' %(t_end - t_start))
+        except StopIteration:
+            print('completed')
+            engine.dispose()
+            break
 
 
 
