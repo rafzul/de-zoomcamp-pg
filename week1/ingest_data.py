4be075c0550659f15de696b6eab04089db118149
rafzul <rafi.zulfahmi@mail.ugm.ac.id>
2022-04-17 23:17:56 +0700

    venv

--- week1/ingest_data.py
+++ week1/ingest_data.py
@@ -7,7 +7,7 @@ import argparse
 from time import time 
 
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine    
 
 def main(params):
     user = params.user
