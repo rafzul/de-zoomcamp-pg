b434b7a0c1f3a9fdc1969a6d1c36d08c571616ba
rafzul <rafi.zulfahmi@mail.ugm.ac.id>
2022-04-17 23:02:35 +0700

    backup or duplicate original ingest_data.py, remove the original converted notebook line from file

---
+++ week1/ingest_data_converted_from_notebook.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os 
+import argparse
+
+from time import time 
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    table_name = params.table_name
+    url = params.url
+    csv_name = 'output.csv'
+
+    #download the CSV
+    os.system(f"wget {url} -O {csv_name}")
+    #create sqlalchemy postgres engine `database://user:password@host:port/database_name`
+    engine = create_engine(f'postgresql//{user}:{password}@{host}:{port}/{db}')
+    #create iterator object to read csv as chunk
+    deef_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    #get first chunk
+    deef = next(deef_iter)
+    #convert pickup time and dropoff time column to timestamps
+    deef.tpep_pickup_datetime = pd.to_datetime(deef.tpep_pickup_datetime)
+    deef.tpep_dropoff_datetime = pd.to_datetime(deef.tpep_dropoff_datetime)
+    #finally, create table in the database (empty table first, generate only the column). schema is inferred from the fetched df
+    deef.head(0).to_sql(name=table_name, con=engine, if_exists='replace')
+    #get the iteration to get the first chunk of CSV and append to table
+    deef.to_sql(name=table_name, con=engine, if_exists='append')
+
+    #start the loop to get all the remaining chunks of downloaded csv
+    while True:
+        try:
+            #get starting time 
+            t_start = time()
+            #get next chunks
+            deef = next(deef_iter)
+            #convert pickup time & dropoff time column type of next chunk
+            deef.tpep_pickup_datetime = pd.to_datetime(deef.tpep_pickup_datetime)
+            deef.tpep_dropoff_datetime = pd.to_do_datetime(deef.tpep_dropoff_datetime)
+            #get the iteration to get next chunk of CSV and append to table 
+            deef.to_sql(name=table_name, con=engine, if_exists='append')
+            #get end time & print log messages
+            t_end = time()
+            print('inserted another chunk, took %.3f second' %(t_end - t_start))
+        except StopIteration:
+            print('completed')
+            engine.dispose()
+            break
+if __name__ = __main__:
+    #create the parser object
+    parser = argparse.ArgumentParser(desription='process command line arguments')
+
+    #adding arguments
+    parser.add_argument('--user', help='username for Postgres')
+    parser.add_argument('--password', help='password for Postgres')
+    parser.add_argument('--host', help='host for postgres')
+    parser.add_argument('--port', help='port for postgres')
+    parser.add_argument('--db', help='database name for postgres')
+    parser.add_argument('--table_name', help='table name for postgres where we will write the results of csv read loops')
+    parser.add_arguments('--url', help='url of the csv file')
+
+    #getting the arguments that we add in command line when we run the program by parsing all the inputted flag
+    args = parser.parse_args()
+
+    #running the main function with inputted args 
+    main(args)
+
+
+
+
+# The CSV file is very big and Pandas may not be able to handle it properly if the whole thing doesn't fit in RAM. We will only import 100 rows for now.
+df = pd.read_csv('yellow_tripdata_2021-01.csv', nrows=100)
+df
+# We will now create the ***schema*** for the  database. The _schema_ is the structure of the database; in this case it describes the columns of our table. Pandas can output the SQL ***DDL*** (Data definition language) instructions necessary to create the schema.
+# We need to provide a name for the table; we will use 'yellow_taxi_data'
+print(pd.io.sql.get_schema(df, name='yellow_taxi_data'))
+# Note that this only outputs the instructions, it hasn't actually created the table in the database yet.
+# Convert `tpep_pickup_datetime` and `tpep_dropoff_datetime` from text to timestamps.
+df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+print(pd.io.sql.get_schema(df, name='yellow_taxi_data'))
+
+# --------------------------basically yg di atas cuma latihan untuk mendapatkan schema dan ngubah tipe data
+#connect to Postgres and create the table out of outputted DDL using sqlalchemy
+# An ***engine*** specifies the database details in a URI. The structure of the URI is:
+# `database://user:password@host:port/database_name`
+engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+engine.connect()
+# we can now use our engine to get the specific output for Postgres
+print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
+# We will now create an ***iterator*** that will allow us to read the CSV file in chunks and send them to the database. Otherwise, we may run into problems trying to send too much data at once.
+df_iter = pd.read_csv('yellow_tripdata_2021-01.csv', iterator=True, chunksize=100000)
+df_iter
+# Use the `next()` function to get the chunks using the iterator.
+df = next(df_iter)
+df
+# This is a brand new dataframe, so we need to convert the time columns to timestamp format.
+df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+df
+# We will now finally create the table in the database. With `df.head(n=0)` we can get the name of the columns only, without any additional data. We will use it to generate a SQL instruction to generate the table.
+#
+# we need to provide the table name, the connection and what to do if the table already exists
+# we choose to replace everything in case you had already created something by accident before.
+df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
+# You can now use `pgcli -h localhost -p 5432 -u root -d ny_taxi` on a separate terminal to look at the database:
+# 
+#  `\dt` for looking at available tables.
+#  `\d yellow_taxi_data` for describing the new table.
+
+# Let's include our current chunk to our database and time how long it takes.
+get_ipython().run_line_magic('time', "df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')")
+
+# Let's write a loop to write all chunks to the database. Use the terminal with `pgcli` to check the database after the code finishes running.
+from time import time
+
+while True: 
+    try:
+        t_start = time()
+        df = next(df_iter)
+
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+        
+        df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
+
+        t_end = time()
+
+        print('inserted another chunk, took %.3f second' % (t_end - t_start))
+    except StopIteration:
+        print('completed')
+        break
+# And that's it! Feel free to go back to the [notes](../notes/1_intro.md#inserting-data-to-postgres-with-python)
