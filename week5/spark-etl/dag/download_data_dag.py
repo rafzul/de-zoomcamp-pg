b441ee2be227e6eb6292d5ed9285b6ac107ef37e
rafzul <rafi.zulfahmi@mail.ugm.ac.id>
2022-05-07 14:59:08 +0700

    download data dag scripting

--- week5/spark-etl/dag/download_data_dag.py
+++ week5/spark-etl/dag/download_data_dag.py
@@ -1,55 +1,50 @@
-from zmq import TYPE
 from airflow.operators import BashOperator
 from airflow.models import DAG
 # from datetime import datetime, timedelta
-from airflow.utils.dates import days_ago
+from datetime import datetime
 
 
 #setting up Bash parametrization
-TAXI_TYPE=$1
-YEAR=$2
+TAXI_TYPE="yellow"
 URL_PREFIX="https://s3.amazonaws.com/nyc-tlc/trip+data"
 
-#setting up bash script for downloading data (minus the looping, will be done together with the parquetization task for each taxi type/month/year inside dag)
+#getting month and year
+logical_date = "{{ ds }}"
+MONTH = datetime.strptime(logical_date, "%m")
+YEAR = datetime.strptime(logical_date, "%y")
 
-BASH_SCRIPT = (
-            f"
-            set -e 
+#setting up bash script for downloading data (minus the looping, will be done together with the parquetization task for each taxi type/month/   x`year inside dag)
 
-            FMONTH= `printf "%02d" ${MONTH}`
-            URL="${URL_PREFIX}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv"
-            LOCAL_PREFIX="/media/rafzul/Terminal Dogma/nytaxidata/raw/${TAXI_TYPE}/${YEAR}/${MONTH}"
-            LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv"
-            LOCAL_PATH="${LOCAL_PREFIX}/${LOCAL_FILE}"
+default_args = {
+    "owner": "rafzul",
+    "start_date": datetime(2020,1,1),
+    "schedule_interval"="@monthly",
+    "depends_on_past": False,
+    "retries": 1,
+}
 
-            echo "downloading ${URL} to ${LOCAL_PATH}"
-            mkdir -p ${LOCAL_PREFIX}
-            wget ${URL} -O ${LOCAL_PATH}
-
-            echo "compressing ${LOCAL_PATH}"
-            gzip
-            "
-        )
 
 with DAG(
     dag_id="download_dag",
-    schedule_interval="@daily",
     default_args=default_args,
     catchup=False,
-    max_active_runs=1,
+    max_active_runs=3,
     tags=['nytaxi-dag'],
 ) as dag:
 
-    for MONTH in {1..12}:
-        download_data_task = BashOperator(
-            task_id='download_data',
-            bash_command=BASH_SCRIPT,
-            
-        )
+    # for MONTH in {1..12}: ini didefine di schedule_interval buat jaraknya, trus define start_date dan end_date buat start dan mulenya
 
-        if scheme == TRUE:
-            scheme_and_parquetize_task = PythonOperator(
-            
+    # for TAXI_TYPE in {yellow,green}:
+
+    download_data_task = BashOperator(
+        task_id='download_data',
+        bash_command="../scripts/download_data.sh",
+        params= {"TAXI_TYPE: {TAXI_TYPE}", "YEAR": {YEAR}, "MONTH": {MONTH}},        
+    )
+
+    if scheme == TRUE:
+        scheme_and_parquetize_task = PythonOperator(
+  
         )
-        else:
+    else:
         
